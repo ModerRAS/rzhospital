83df6e986c7da1cae9b11c0da702c6e1224a2ee0
ModerRAS <adqewsx@gmail.com>
2018-12-01 15:32:42 +0800

    AutoSave --AutoGit

--- RZ_hospital_Spider.py
+++ RZ_hospital_Spider.py
@@ -1,8 +1,38 @@
 #!/bin/env python
-import time
-
 import requests
 from bs4 import BeautifulSoup
+import os
+import time
+import subprocess
+import re
+import config
+
+sleep_time = 60
+DIR = config.DIR
+
+
+def git():
+    for i in DIR:
+        os.chdir(i)
+        try:
+            add = subprocess.check_output("git add .", shell=True)
+        except subprocess.CalledProcessError as e:
+            add = e.output
+        pattern = "nothing to commit, working tree clean"
+        try:
+            commit = subprocess.check_output("git commit -m \"AutoSave --AutoGit\"", shell=True)
+        except subprocess.CalledProcessError as e:
+            commit = e.output
+
+        if re.search(pattern, commit.decode('utf-8')) is not None:
+            return 0
+        else:
+            print(time.asctime(time.localtime(time.time())))
+            print(add.decode('utf-8'))
+            print(commit.decode('utf-8'))
+            os.system("git push")
+            print("One loop end.")
+            return 0
 
 
 def rss_create(data):
@@ -63,10 +93,13 @@ def more_info(url: str):
     return soup.select(".list-content")[0].prettify()
 
 
-def main():
+def loop():
     rss = spider()
     rss_save(rss, "feed.xml")
+    git()
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        loop()
+        time.sleep(sleep_time)
--- rzhs.sh
+++ rzhs.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 python3 RZ_hospital_Spider.py
-python3 AutoGit.py
-sleep 1m
-./rzhs.sh &
